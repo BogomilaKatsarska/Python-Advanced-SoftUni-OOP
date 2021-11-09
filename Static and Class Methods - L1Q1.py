a45f51992d156eef8988a25b1f2e801851fd8464
BogomilaKatsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2021-11-09 16:09:50 +0200

    Add files via upload
    01.Calculator: https://judge.softuni.org/Contests/Practice/Index/2430#0

---
+++ Static and Class Methods - L1Q1.py
@@ -0,0 +1,31 @@
+from functools import reduce
+
+
+class Calculator:
+    @staticmethod
+    def add(*args):
+        return sum(args)
+
+    @staticmethod
+    def multiply(*args):
+        return reduce(lambda x, y: x * y, args)
+
+    @staticmethod
+    def divide(*args):
+        return reduce(lambda x, y: x / y, args)
+
+    @staticmethod
+    def subtract(*args):
+        return reduce(lambda x, y: x - y, args)
+
+
+#Functools -> Reduce:
+# At first step, first two elements of sequence are picked and the result is obtained.
+# Next step is to apply the same function to the previously attained result and the number just succeeding the second element and the result is again stored.
+# This process continues till no more elements are left in the container.
+# The final returned result is returned and printed on console.
+
+print(Calculator.add(5, 10, 4))
+print(Calculator.multiply(1, 2, 3, 5))
+print(Calculator.divide(100, 2))
+print(Calculator.subtract(90, 20, -50, 43, 7))
